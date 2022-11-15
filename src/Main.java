5f84737fcdd680a1a4ee753b07e80c69d7541de2
medhaholla <holla.medha@gmail.com>
2022-11-15 15:04:35 +0530

    The user enters the command exit it comes out of the loop

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("In main page");
         System.out.println("Enter the command");
+        Scanner scanner = new Scanner(System.in);
+        HashMap<String,Integer> urlCountMap = new HashMap<>();
+        HashMap<String,String> urlShortUrlMap = new HashMap<>();
+
+        while(true){
+            String inputCommmand = scanner.nextLine();
+            String[] inputCommandArgs  = inputCommmand.split("\\s+");
+            String command = inputCommandArgs[0];
+            if(command.equals("exit")){
+                break;
+            }
+        }
+
     }
 }
