27de3d9d597e12549b11278ec16dc6942702e53b
medhaholla <holla.medha@gmail.com>
2022-11-15 15:13:44 +0530

    The command storeurl and geturl

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.UUID;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,6 +16,21 @@ public class Main {
             String command = inputCommandArgs[0];
             if(command.equals("exit")){
                 break;
+            }
+            switch(command){
+                case "storeurl":
+                    if(!inputCommandArgs[1].isEmpty()) {
+                        urlCountMap.put(inputCommandArgs[1], 0);
+                        urlShortUrlMap.put(inputCommandArgs[1], UUID.randomUUID().toString());
+                    }
+                    break;
+                case "get":
+                    if(!inputCommandArgs[1].isEmpty() && urlCountMap.containsKey(inputCommandArgs[1])) {
+                        int currentCount = urlCountMap.get(inputCommandArgs[1]);
+                        urlCountMap.put(inputCommandArgs[1], currentCount + 1);
+                        System.out.println(urlShortUrlMap.get(inputCommandArgs[1]));
+                    }
+                    break;
             }
         }
 
